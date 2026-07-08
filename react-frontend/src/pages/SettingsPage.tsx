350af5e0af162a8513839090d0e15851bf14f7bd
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-07-08 19:36:47 -0300

    Apply dark theme to SettingsPage

--- react-frontend/src/pages/SettingsPage.tsx
+++ react-frontend/src/pages/SettingsPage.tsx
@@ -2,9 +2,9 @@ import { AccountSettingsForm } from '../components/AccountSettingsForm';
 
 export function SettingsPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen items-center justify-center bg-neutral-950">
       <div className="flex flex-col gap-4">
-        <h1 className="text-xl font-semibold">Find My Account</h1>
+        <h1 className="text-xl font-semibold text-neutral-100">Find My Account</h1>
         <AccountSettingsForm />
       </div>
     </main>

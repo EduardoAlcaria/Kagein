fe3071d2b087d45c1ffc6379ce4181a946116c11
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2026-07-15 01:10:21 -0300

    Redesign DashboardPage as card-based dashboard

--- react-frontend/src/pages/DashboardPage.tsx
+++ react-frontend/src/pages/DashboardPage.tsx
@@ -7,6 +7,7 @@ import { MapPanel } from '../components/MapPanel';
 import { LocationHistoryList } from '../components/LocationHistoryList';
 import { PredictionTotalizers } from '../components/PredictionTotalizers';
 import { RecentAlertsWidget } from '../components/RecentAlertsWidget';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 
 export function DashboardPage() {
   const { data: people } = usePeople();
@@ -15,23 +16,48 @@ export function DashboardPage() {
   const { data: locations } = usePersonLocations(selectedPersonId);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <PredictionTotalizers people={people ?? []} />
-      <div className="flex h-[calc(100vh-49px)]">
-        <PeopleSidebar
-          people={people ?? []}
-          selectedPersonId={selectedPersonId}
-          onSelectPerson={setSelectedPersonId}
-        />
-        <div className="flex-1">
-          <MapPanel
-            people={people ?? []}
-            selectedPersonId={selectedPersonId}
-            onSelectPerson={setSelectedPersonId}
-            trail={locations ?? []}
-          />
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-6">
+        <Card className="lg:col-span-2">
+          <CardHeader>
+            <CardTitle>Map</CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <div className="h-[420px] overflow-hidden rounded-b-xl">
+              <MapPanel
+                people={people ?? []}
+                selectedPersonId={selectedPersonId}
+                onSelectPerson={setSelectedPersonId}
+                trail={locations ?? []}
+              />
+            </div>
+          </CardContent>
+        </Card>
+        <div className="flex flex-col gap-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>People</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <PeopleSidebar
+                people={people ?? []}
+                selectedPersonId={selectedPersonId}
+                onSelectPerson={setSelectedPersonId}
+              />
+            </CardContent>
+          </Card>
+          {selectedPersonId !== null && (
+            <Card>
+              <CardHeader>
+                <CardTitle>History</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <LocationHistoryList locations={locations ?? []} />
+              </CardContent>
+            </Card>
+          )}
         </div>
-        {selectedPersonId !== null && <LocationHistoryList locations={locations ?? []} />}
       </div>
       <RecentAlertsWidget alerts={alerts ?? []} />
     </div>
